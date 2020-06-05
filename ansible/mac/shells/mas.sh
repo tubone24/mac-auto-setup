425c1428036842f9c2e20942aca5217b48027856
tubone <meitantei-conan@live.jp>
2020-06-05 02:46:40 +0000

    mas

---
+++ ansible/mac/shells/mas.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+APPID=$1
+
+## Argument check
+test $# -eq 1 || exit 2
+
+## App check
+mas list 2> /dev/null | grep -q "^${APPID} " && exit 0
+
+## App install
+mas install ${APPID} > /dev/null 2>&1 || exit 3
+
+## Normal termination
+exit 1
